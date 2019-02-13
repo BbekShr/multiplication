5ca83fc2053715c0613f08e68d6a435bfd91aeeb
Bibek <bbeksh@gmail.com>
2019-02-13 11:50:58 -0600

    Final version. Added working slider and table view working accordingly

--- multiplication/ViewController.swift
+++ multiplication/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+    @IBOutlet weak var sliderLabelText: UILabel!
+    @IBOutlet weak var sliderView: UISlider!
+    
+    @IBOutlet weak var tableViewUpdate: UITableView!
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        sliderLabelText.text = "\(currentValue)"
+        tableViewUpdate.reloadData()
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numOfRow = 50
+        return numOfRow
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let count = Int(sliderView.value)
+        var multiplicationTable: [String] = []
+        
+        for i in 1...50 {
+            multiplicationTable.append("\(count) X \(i) = \(count * i)")
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = multiplicationTable[indexPath.row]
+        
+        return cell
+    }
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
 }
-
