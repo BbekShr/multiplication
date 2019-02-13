da1cb4cd6118f07699a0e821237ce85bbb56ad72
Bibek <bbeksh@gmail.com>
2019-02-13 00:38:08 -0600

    Initial Commit

---
+++ multiplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiplication
+//
+//  Created by Bibek Shrestha on 2/13/19.
+//  Copyright © 2019 Bibek Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
